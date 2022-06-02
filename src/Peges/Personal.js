182a31e11c449c20c189ce61776d414d1eef3824
AnatoliiZagadaev <anatolii.zagadaev@gmail.com>
2022-06-02 21:15:22 +0300

    commit

--- src/Peges/Personal.js
+++ src/Peges/Personal.js
@@ -1,76 +1,13 @@
-import React, { useState } from "react";
-import Tables from '../Components/PersonalTaletView';
-import Modal from '../Components/FormPersonal.js';
+import React from "react";
+import DataPersonl from '../Components/DataPersonal'
 
 function Personal(){
-  const [DataPerson, setDataPerson] = useState([
-    {
-      firstName:'Анатолій',
-       surname:'Загадаєв', 
-       age:'34 ',
-        gender:'male',
-        id: 1
-    },
-    {
-      firstName:'Анатолій',
-       surname:'Загадаєв', 
-       age:'36 ',
-        gender:'male',
-        id: 2
-    },
-    {
-      
-      firstName:'Анатолій',
-       surname:'Загадаєв', 
-       age:'30 ',
-        gender:'male',
-        id: 3
-    }
-  ]);
-
-  DataPerson.sort((a, b) => (a.age > b.age) ? 1 : -1)
-    console.log(DataPerson)
-
-
- 
- 
-  const [editDataPerson] = useState(null);
-  function deleteDataPerson(dataperson) {
-    setDataPerson(DataPerson.filter((p) => p !== dataperson));
-  }
-
-  const addDataPerson = (dataperson) => {
-    setDataPerson([...DataPerson, dataperson]);
-  };
-
-  const saveDataPerson = (dataperson) => {
-    setDataPerson(
-      DataPerson.reduce((acc, p) => {
-        if (p.id === dataperson.id) acc.push(dataperson);
-        else acc.push(p);
-        return acc;
-      }, [])
-    );
-  };
-  const handleSort = () => {
-    const sorted = [...DataPerson].sort(((a, b) => b.age - a.age) ? 1 : -1)
-    console.log("sorted");
-    setDataPerson(sorted)
-  }
-  const handleReverse =()=>{
-    const sortedReverse = [...DataPerson].reverse();
-    setDataPerson(sortedReverse)
-  }
  
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Data Person</h1>
-      <Tables
-        DataPerson={DataPerson}
-        delete={deleteDataPerson}
-      />
-      <Modal sorted={handleSort} add={addDataPerson} save={saveDataPerson} sortedReverse={handleReverse} dataperson={editDataPerson} />
-    </div>
+    <section>
+      <h1>Data Person</h1>
+      <DataPersonl/>
+    </section>
   );
 }
 
