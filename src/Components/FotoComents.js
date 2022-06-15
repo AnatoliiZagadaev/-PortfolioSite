8de52a1c86941ae26c9cf61125bbf33ba972e65f
AnatoliiZagadaev <103272162+AnatoliiZagadaev@users.noreply.github.com>
2022-06-15 12:55:29 +0300

    Update FotoComents.js

--- src/Components/FotoComents.js
+++ src/Components/FotoComents.js
@@ -2,31 +2,28 @@ import React from 'react';
 import '../Components/Form.css';
 
 class FotoComments extends React.Component{
-  constructor(props) {
+ constructor(props) {
 		super(props);
 		this.state = {comments: [], comment: ''};
 	}
 	addComment = (event) => {
-		const comments= [...this.state.comments]
-		comments.push(this.state.comment)
-		this.setState({comments })
+		this.setState(({comments, comment}) => ({comments: [...comments, comment] }))
 		event.preventDefault();
 	}
 	handleChange=(event)=>{
 		this.setState({comment: event.target.value});
 	}
-
 	render() {
 		return (
 			<div>	
 			<form  onSubmit={this.addComment}>
-			<textarea value={this.state.comment}	onChange={this.handleChange}/>
+			<textarea value={this.state.comment} onChange={this.handleChange}/>
 			<input type="submit" className='actionBtn'/>
 			</form>
-		         <ul>
+		        <ul>
 			 {this.state.comments.slice(0,1).map((item, index) => {return <li key={index}>{item}</li>})}
 			</ul>
-		</div>
+		  </div>
 		);
 }
 }
