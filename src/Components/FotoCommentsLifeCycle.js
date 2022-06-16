3ba49de0c5ee4b2dddf4f9eae3762867e63ac4ab
AnatoliiZagadaev <anatolii.zagadaev@gmail.com>
2022-06-16 23:26:04 +0300

    Дописав prevState

--- src/Components/FotoCommentsLifeCycle.js
+++ src/Components/FotoCommentsLifeCycle.js
@@ -1,59 +1,66 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-class FotoCommentsLifeCycle extends Component {       
-  
-    state = {
-    comments: [],
-    comment: ''
-
+export default class FotoCommentsLifeCycle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [],
+      comment: ''
+    };
   }
 
-  componentDidMount=()=>{
+  componentDidMount() {
     if (localStorage.getItem('state')) {
-      this.setState({ ...JSON.parse(localStorage.getItem('state')) })
+      this.setState({ ...JSON.parse(localStorage.getItem('state')) });
     }
   }
-  componentWillUnmount=()=>{
-    clearInterval(this.localStorage)
-  }
-  addComment=()=> {
-    this.setState({
-      comments: [
-        ...this.state.comments,
-        {
-          id: this.state.comments.length ? this.state.comments.reduce((p, c) => p.id > c.id ? p : c).id + 1 : 1, // max id +1
-          comment: this.state.comment,
-          
-        }
-      ],
+
+  addComment = () => {
+    this.setState(
+      (prevState) => ({
+        comments: [
+          ...prevState.comments,
+          {
+            id: prevState.comments.length ? prevState.comments.reduce((p, c) => (p.id > c.id ? p : c)).id + 1 : 1, // max id +1
+            comment: this.state.comment,
+          }
+        ],
         comment: ''
-    }, () => localStorage.setItem('state', JSON.stringify(this.state)))
-  }
+      }),
+      () => {
+        return localStorage.setItem('state', JSON.stringify(this.state));
+      }
+    );
+  };
 
   removeComment = (id) => {
-    this.setState({
-      comments: this.state.comments.filter(comment => comment.id !== id)
-    }, () => localStorage.setItem('state', JSON.stringify(this.state)))
-  }
+    this.setState((prevState) => ({
+      comments: prevState.comments.filter((comment) => comment.id !== id)
+    }), () => localStorage.setItem('state', JSON.stringify(this.state)));
+  };
 
   handleChange = (event) => {
     this.setState({
-        [event.target.name]: event.target.value,
+      [event.target.name]: event.target.value,
     });
-  }
+  };
 
-  render(){
-    return(
+  render() {
+    const { comment } = this.state;
+    const { comments } = this.state;
+    return (
       <div>
-          <textarea name="comment" value={this.state.comment}onChange={this.handleChange}></textarea>
-          <button onClick={this.addComment}>Add Comment</button>
-        {this.state.comments.map(comment => <div key={comment.id}>{comment.id}.{comment.comment}
-          <button onClick={() => this.removeComment(comment.id)}>Remove Comment</button>
-        </div>)}
+        <textarea name="comment" value={comment} onChange={this.handleChange} />
+        <button onClick={this.addComment}>Add Comment</button>
+        {comments.map((comment) => (
+          <div key={comment.id}>
+            {comment.id}
+            .
+            {comment.comment}
+            <button onClick={() => this.removeComment(comment.id)}>Remove Comment</button>
+          </div>
+        ))}
       </div>
-    )
+    );
   }
 }
-
-
-export default FotoCommentsLifeCycle
